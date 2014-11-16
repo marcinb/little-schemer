ceae367316415aba376d34c8a9bc151f88909c2e
Marcin Balinski <marcin.balinski@wimdu.com>
2014-11-16 21:31:03 +0100

    Splits list transformations from higher order functions excercises.

--- lists.scm
+++ lists.scm
@@ -26,26 +26,6 @@
               (eq? e (car lat))
               (member? e (cdr lat)))))))
 
-;; Takes number as an argument.
-;; Returns function that takes other number as an argument.
-;; The returned fuction adds both numbers.
-(define make-adder
-  (lambda (x)
-    (lambda (y)
-      (+ x y))))
-
-;; Takes functions `f` ang `g` as arguments.
-;; Returns a function which takes any S-expression `x` as argument
-;; the returned fuction applies function `g` to argument `x` and then applies `f` to result.
-(define compose
-  (lambda (f g)
-    (lambda (x)
-      (f (g x)))))
-
-(define add10
-  (lambda (x)
-    ((compose (make-adder 5) (make-adder 5)) x)))
-
 ;; Takes atom and list as arguments.
 ;; Returns new list being a copy of given list with
 ;; the first occurence of given atom removed.
