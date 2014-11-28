4e9aca6616c0e701a07d4bd53878420da1b4bd6c
Marcin Balinski <marcin.balinski@wimdu.com>
2014-11-28 17:15:55 +0100

    Defines 'pick' function returning n-th element from given list.

--- numbers.scm
+++ numbers.scm
@@ -108,3 +108,10 @@
       (else
 	(add1 (length (cdr lat)))))))
 
+;; Returns n-th element of given list.
+(define pick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (car lat))
+      (else
+	(pick (sub1 n) (cdr lat))))))
