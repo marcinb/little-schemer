a23b1a9d1e01fb2d6068d746aefa2db67ce66b23
Marcin Balinski <marcin.balinski@wimdu.com>
2014-11-28 17:16:26 +0100

    Defines 'rempick' finction removing n-th element from given list.

--- numbers.scm
+++ numbers.scm
@@ -115,3 +115,15 @@
       ((zero? (sub1 n)) (car lat))
       (else
 	(pick (sub1 n) (cdr lat))))))
+
+;; Takes non-negative number and list as arguments.
+;; Returns copy of given list with n-th element removed.
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (cdr lat))
+      (else
+	(cons 
+	  (car lat) 
+	  (rempick (sub1 n) (cdr lat)))))))
+
