cb38d5747264803ee36759b3eb2e678819264fe1
Marcin Balinski <marcin.balinski@wimdu.com>
2014-11-28 16:05:08 +0100

    adds missing comments

--- numbers.scm
+++ numbers.scm
@@ -1,11 +1,14 @@
+;; Increments given number by 1.
 (define add1
   (lambda (n)
     (+ n 1)))
 
+;; Decrements given number by 1.
 (define sub1
   (lambda (n)
     (- n 1)))
 
+;; Adds two non negative numbers.
 (define add
   (lambda (n m)
     (cond
@@ -13,6 +16,7 @@
       (else
 	(add1 (add n (sub1 m)))))))
 
+;; Subtracts two non negative numbers.
 (define sub
   (lambda (n m)
     (cond
