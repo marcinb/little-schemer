91732acbecac0cc72d0fec180eecf788916d33e0
Marcin Balinski <marcin.balinski@wimdu.com>
2014-11-28 16:06:55 +0100

    Defines multiplication, division and power functions

--- numbers.scm
+++ numbers.scm
@@ -23,3 +23,28 @@
       ((zero? m) n)
       (else
 	(sub1 (sub n (sub1 m)))))))
+
+;; Multiplies two non negative numbers.
+(define mult
+  (lambda (n m)
+    (cond
+      ((zero? m) 0)
+      (else
+	(add n (mult n (sub1 m)))))))
+
+;; Takes two non negative numbers as arguments.
+;; Returns first number taken to the power of second one
+(define pow
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else 
+	(mult n (pow n (sub1 m)))))))
+
+;; Divides two non negative natural numbers
+(define div
+  (lambda (n m)
+    (cond
+      ((lower? n m) 0)
+      (else
+	(add1 (div (sub n m) m))))))
